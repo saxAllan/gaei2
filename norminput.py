bd54b44ef880190062e997dcf0c6a3402c69aca8
saxAllan <asahi_shimizu@outlook.com>
2020-06-18 12:10:53 +0900

    正規化をするか否かを選べるように

--- norminput.py
+++ norminput.py
@@ -2,13 +2,14 @@
 #最終的なデータはdata[][]
 
 print("\n========================================")
-print("  norminput Ver. 3.3 (20200601)")
+print("  norminput Ver. 3.4 (20200618)")
 print("========================================\n")
 
 from operator import itemgetter
 
 #読み込み
 filename_i = input("入力ファイル名（拡張子は不要）：")
+seikika = input("正規化しますか？（Y/N）：")
 print(filename_i + ".datを読み込んでいます...  ")
 f = open(filename_i + ".dat", "r")
 lines = f.readlines()
@@ -27,13 +28,47 @@ right = b[len(b)-1][0]
 delete = []
 
 #正規化処理
-while(1):
-    for i in range(len(orgdata)):
-        if orgdata[i][0] == left or orgdata[i][0] == right or orgdata[i][1] == bottom or orgdata[i][1] == top:
-            delete.append(i)
-    for i in range(len(delete)):
-        orgdata.pop(delete[i]-i)
+if seikika == "Y" or seikika == "y":
+    while(1):
+        for i in range(len(orgdata)):
+            if orgdata[i][0] == left or orgdata[i][0] == right or orgdata[i][1] == bottom or orgdata[i][1] == top:
+                delete.append(i)
+        for i in range(len(delete)):
+            orgdata.pop(delete[i]-i)
 
+        # 座標カウント
+        y = orgdata[0][1]
+        i = 0
+        j = 0
+        count_x = 0
+        count_y = 0
+        while orgdata[i][1] == y:
+            count_x += 1
+            i += 1
+        count_y = len(orgdata) // count_x
+        data = []
+        for i in range(count_x):
+            data.append([])
+
+        size_org = len(orgdata)
+
+        # 正規化チェック
+        print("x=", count_x, "y=", count_y)
+        if count_x * count_y == size_org:
+            print("正規化完了")
+            break
+        else:
+            print("続行")
+            from operator import itemgetter
+            b = sorted(orgdata, key=itemgetter(0))
+
+            bottom = orgdata[0][1]
+            top = orgdata[len(orgdata)-1][1]
+            left = b[0][0]
+            right = b[len(b)-1][0]
+            delete = []
+            #while ここまで
+else:
     # 座標カウント
     y = orgdata[0][1]
     i = 0
@@ -54,18 +89,7 @@ while(1):
     print("x=", count_x, "y=", count_y)
     if count_x * count_y == size_org:
         print("正規化完了")
-        break
-    else:
-        print("続行")
-        from operator import itemgetter
-        b = sorted(orgdata, key=itemgetter(0))
 
-        bottom = orgdata[0][1]
-        top = orgdata[len(orgdata)-1][1]
-        left = b[0][0]
-        right = b[len(b)-1][0]
-        delete = []
-        #while ここまで
 
 #2次元配列化
 print("二次元配列化処理中...")
