f3f2f38ba27dfa05d455b79215003453402672f3
saxAllan <asahi_shimizu@outlook.com>
2020-06-18 11:08:31 +0900

    merge from VRML/play_to_VRML.py

--- play_n.py
+++ play_n.py
@@ -5,3 +5,15 @@ import dijkstra
 d = dijkstra.Dijkstra(20, (0, 0), (8, 9), 20, [(2, 2), (2, 3)])
 d.search()
 print(d.routeHeight)
+
+out=""
+for i in range(len(d.routeHeight)):
+    print("\r",i,end="  ")
+    out += str(d.routeHeight[i][0])+" "
+    out += str(d.routeHeight[i][1])+" "
+    out += str(d.routeHeight[i][2])+"\n"
+
+f=open("route.txt",mode="w")
+f.write(out)
+f.close()
+print("route.txt に追記しました")
