f3b87081dda4ee5932334b0456a7f4fbad0094a1
Apple-Tree-55 <naopenx5@gmail.com>
2020-06-24 18:23:57 +0900

    少し改善

--- Yuki_K/t2.py
+++ Yuki_K/t2.py
@@ -7,7 +7,7 @@ ySIZE = int(input("ySIZE:"))
 zSIZE = int(input("zSIZE:"))
 N = xSIZE*ySIZE*zSIZE
 start = 0  # NUM形式
-end = 3  # NUM形式
+end = 6  # NUM形式
 
 
 def cd(n):  # 座標に直す
@@ -21,25 +21,27 @@ def num(x, y, z):  # 番号に直す
 def init():
     ar = np.zeros((N, N))
     for i in range(N):
-        chkList = [i-1, i+1, i-xSIZE, i+xSIZE,
-                   i-(xSIZE*ySIZE), i+(xSIZE*ySIZE)]
+        chkList=[]
+        for a in range(-1,2):
+            for b in range(-1,2):
+                for c in range(-1,2):
+                    ap = i+a+b*xSIZE+c*(xSIZE*ySIZE)
+                    if 0<=ap and ap<N and ap!=i:
+                        chkList.append(ap)
         location = cd(i)
-        print(i, chkList, end="")
         for e in chkList:
             check = cd(e)
-            if check[2] < 0 or N <= check[2]:
-                continue
-            print(e, end="")
             cnt = 0
             for s in range(3):
-                if abs(location[s]-check[s]) == 1:
+                dist = abs(location[s]-check[s])
+                if dist == 1:
                     cnt += 1
+                elif dist>1:
+                    cnt=0
+                    break
             ar[i][e] = np.sqrt(cnt)
-        print("")
     return ar
 # 障害物部分を0にする必要がある
-# 隣接セルのみ1にする処理
-
 
 ar = init()
 
@@ -47,7 +49,7 @@ print(ar)
 
 d, p = shortest_path(ar, indices=start, return_predecessors=True)
 
-# print(p)
+print(p)
 
 
 def get_path_row(start, goal, pred_row):
@@ -62,4 +64,4 @@ def get_path_row(start, goal, pred_row):
     return path[::-1]
 
 
-#print(get_path_row(start, end, p))
+print(get_path_row(start, end, p))
