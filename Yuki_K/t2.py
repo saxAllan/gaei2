f3d3ff3e4f1ee6ed0c05e49d2ccd39b1d68a998b
Apple-Tree-55 <naopenx5@gmail.com>
2020-07-02 14:24:32 +0900

    Scipy疎行列

--- Yuki_K/t2.py
+++ Yuki_K/t2.py
@@ -1,13 +1,21 @@
+# -------- NOTE: Naming 'scipy.py' causes an error -------
+
 import numpy as np
 from scipy.sparse.csgraph import shortest_path, floyd_warshall, dijkstra, bellman_ford, johnson
-from scipy.sparse import csr_matrix
+from scipy.sparse import lil_matrix, csr_matrix, coo_matrix
+from scipy import sparse
+import sys
 
-xSIZE = int(input("xSIZE:"))
-ySIZE = int(input("ySIZE:"))
-zSIZE = int(input("zSIZE:"))
+xSIZE = 100
+ySIZE = 100
+zSIZE = 100
 N = xSIZE*ySIZE*zSIZE
 start = 0  # NUM形式
 end = 6  # NUM形式
+obst = [[1, 0, 0], [2, 2, 1]]
+data = []
+row = []
+col = []
 
 
 def cd(n):  # 座標に直す
@@ -18,50 +26,63 @@ def num(x, y, z):  # 番号に直す
     return x+y*xSIZE+z*(xSIZE*ySIZE)
 
 
+def makeData(d, i, j):
+    if d > 0:
+        data.append(d)
+        row.append(i)
+        col.append(j)
+
+
 def init():
-    ar = np.zeros((N, N))
     for i in range(N):
-        chkList=[]
-        for a in range(-1,2):
-            for b in range(-1,2):
-                for c in range(-1,2):
-                    ap = i+a+b*xSIZE+c*(xSIZE*ySIZE)
-                    if 0<=ap and ap<N and ap!=i:
-                        chkList.append(ap)
+        if i % 10000 == 0:
+            sys.stdout.write("\r処理中: "+str((int)(i/N*100))+"%")
+            sys.stdout.flush()
+        chkList = []
         location = cd(i)
-        for e in chkList:
-            check = cd(e)
-            cnt = 0
-            for s in range(3):
-                dist = abs(location[s]-check[s])
-                if dist == 1:
-                    cnt += 1
-                elif dist>1:
-                    cnt=0
-                    break
-            ar[i][e] = np.sqrt(cnt)
-    return ar
+        for a in range(-1, 2):
+            for b in range(-1, 2):
+                for c in range(-1, 2):
+                    ap = i+a+b*xSIZE+c*(xSIZE*ySIZE)
+                    if i < ap and ap < N:
+                        check = cd(ap)
+                        cnt = 0
+                        for s in range(3):
+                            dist = abs(location[s]-check[s])
+                            if dist == 1:
+                                cnt += 1
+                            elif dist > 1:
+                                cnt = 0
+                                break
+                        makeData(np.sqrt(cnt), i, ap)
+        # print(chkList)
+    return coo_matrix((data, (row, col)), (N, N)).tocsr()
 # 障害物部分を0にする必要がある
 
-ar = init()
 
-print(ar)
-
-d, p = shortest_path(ar, indices=start, return_predecessors=True)
-
-print(p)
-
-
-def get_path_row(start, goal, pred_row):
+def path(ar):
+    #d, p = shortest_path(ar, indices=start, return_predecessors=True)
+    d, p = shortest_path(ar, indices=start, directed=False,
+                         return_predecessors=True)
     path = []
-    i = goal
+    i = end
     while i != start and i >= 0:
         path.append(i)
-        i = pred_row[i]
+        i = p[i]
     if i < 0:
         return []
     path.append(i)
-    return path[::-1]
+    # ☆return -> path[::-1]
+    ans = path[::-1]
+    for j in range(len(ans)):
+        ans[j] = cd(ans[j])
+    return ans
 
 
-print(get_path_row(start, end, p))
+ar = init()
+
+print("")
+
+#print(ar)
+
+print(path(ar))
