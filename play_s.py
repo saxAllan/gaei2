9a53e54cb332d2721969627060550fc1372e7dd6
saxAllan <asahi_shimizu@outlook.com>
2020-07-08 15:56:30 +0900

    remove old code

--- play_s.py
+++ play_s.py
@@ -18,22 +18,8 @@ dist_x = int(input("x:"))
 dist_y = int(input("y:"))
 dist_z = int(input("z:"))
 
-'''
-if (start_z > dist_z):
-    search_z = start_z
-else:
-    search_z = dist_z
-'''
-
 obst = []
 obst_count = 0
-
-'''
-for i in range(norminput.count_x):
-    for j in range(norminput.count_y):
-        if norminput.data[i][j][0] >= search_z:
-            obst.append((i, j)) #高度 search_z におけるobst（2次元）
-'''
 
 for i in range(norminput.count_x):
     for j in range(norminput.count_y):
@@ -47,8 +33,3 @@ for i in range(norminput.count_x):
 
 s = Sci.SciSearch([50, 50, 50], [start_x, start_y, start_z], [dist_x, dist_y, dist_z], obst, 1) #(サイズ,始点,終点,障害物,モード)
 
-'''
-d = dijkstra.Dijkstra(norminput.count_x, (start_x, start_y), (dist_x, dist_y), dist_z - start_z, obst) #縦横, start, dist, 高度差, obst
-d.search()
-print(d.routeHeight)
-'''
