11dd7f3bccd15db4fa7f734e02a6416f795da011
saxAllan <asahi_shimizu@outlook.com>
2020-07-12 14:57:09 +0900

    support for new SciPy

--- play_s.py
+++ play_s.py
@@ -1,10 +1,11 @@
 import numpy as np
+import math
 import norminput
 import dijkstra
 import Sci
 
 print("\n========================================")
-print("  play Ver. 1.0 (20200708)")
+print("  play Ver. 1.1 (20200712)")
 print("========================================\n")
 
 print("現在地の座標を入力：")
@@ -23,10 +24,8 @@ obst_count = 0
 for i in range(norminput.count_x):
     for j in range(norminput.count_y):
         if norminput.data[i][j][0] != 0:
-            obst.append([])
-            obst[obst_count].append([i, j, 0])
-            obst[obst_count].append([i, j, int(norminput.data[i][j][0]) + 1])
-            obst_count += 1
+            obst.append([i, j, math.ceil(norminput.data[i][j][0])])
+print(len(obst))
 
 s = Sci.SciSearch([50, 50, 50], [start_x, start_y, start_z], [dist_x, dist_y, dist_z], obst, 1) #(サイズ,始点,終点,障害物,モード)
 
