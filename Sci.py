30aa5b06e24304af711dddf157599ddefcdca211
Apple-Tree-55 <naopenx5@gmail.com>
2020-07-12 15:37:57 +0900

    ファイル読み込みなど

--- Sci.py
+++ Sci.py
@@ -3,6 +3,7 @@ from scipy.sparse.csgraph import shortest_path, floyd_warshall, dijkstra, bellma
 from scipy.sparse import lil_matrix, csr_matrix, coo_matrix
 from scipy import io, sparse
 import sys
+import os
 
 
 class SciSearch:
@@ -14,12 +15,17 @@ class SciSearch:
 
     def _setObst(self):
         lil = self.csr.tolil()
-        for o in self.obst:
+        for idx, o in enumerate(self.obst):  # ステータス表示(7/11更新)
+            sys.stdout.write(
+                "\r障害物設定中: "+str((int)(idx/len(self.obst)*100))+"%")
+            sys.stdout.flush()
             for i in range(o[2]+1):  # obst入力方法更新！(7/9更新)
                 obstNum = self._num(o[0], o[1], i)
                 lil[:, obstNum] = 0
                 lil[obstNum, :] = 0
         self.csr = lil.tocsr()  # スライスができる！！！！！(7/9更新)
+        sys.stdout.write("\r障害物設定完了: 100%             \n")
+        sys.stdout.flush()
 
     def _appendData(self, d, i, j):
         if d > 0:
@@ -51,7 +57,7 @@ class SciSearch:
         sys.stdout.write("\r生成完了: 100%             \n")
         sys.stdout.flush()
 
-    def __init__(self, cd, start, end, obst, mode):
+    def __init__(self, cd, start, end, obst, datName):
         self.xSIZE = cd[0]
         self.ySIZE = cd[1]
         self.zSIZE = cd[2]
@@ -64,18 +70,19 @@ class SciSearch:
         self.row = []
         self.col = []
 
-        if mode == 1:  # １から生成＋ファイル書き込み（mode=1)(7/9更新)
+        fileName = "mat_" + str(self.xSIZE) + "_" + str(self.ySIZE) + \
+            "_" + str(self.zSIZE) + "_" + datName + ".mat"
+
+        if os.path.exists(fileName):  # ファイルから読み込み（ファイルが存在）(7/12更新)
+            self.csr = io.loadmat(fileName)["CSR"]
+            print("ファイル読み込み完了:", fileName)
+        else:  # １から生成＋ファイル書き込み（ファイルが存在しない）(7/11更新)
             self._createData()
             self.csr = csr_matrix(
                 (self.data, (self.row, self.col)), (self.N, self.N))
-            io.savemat("mat_" +
-                       str(self.xSIZE) + "_" + str(self.ySIZE) +
-                       "_" + str(self.zSIZE) + ".mat", {"CSR": self.csr})
-        elif mode == 2:  # ファイルから読み込み（mode=2)(7/9更新)
-            self.csr = io.loadmat("mat_" +
-                                  str(self.xSIZE) + "_" + str(self.ySIZE) +
-                                  "_" + str(self.zSIZE) + ".mat")["CSR"]
-        self._setObst()
+            self._setObst()
+            io.savemat(fileName, {"CSR": self.csr})
+            print("ファイル書き込み完了:", fileName)
 
     def search(self):  # Scipy経路探索
         d, p = shortest_path(self.csr, indices=self.start, directed=False,
