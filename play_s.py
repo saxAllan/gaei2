6a6853408ec6fa444246f17136d92a0608f45c57
saxAllan <asahi_shimizu@outlook.com>
2020-07-23 01:21:53 +0900

    change method of generate obst

--- play_s.py
+++ play_s.py
@@ -4,7 +4,7 @@ import norminput
 import Sci
 
 print("\n========================================")
-print("  play Ver. 1.1 (20200712)")
+print("  play Ver. 1.3 (20200722)")
 print("========================================\n")
 
 print("現在地の座標を入力：")
@@ -22,7 +22,17 @@ obst_count = 0
 
 for i in range(norminput.count_x):
     for j in range(norminput.count_y):
-        if norminput.data[i][j][0] != 0:
+        if 0 < i and i < norminput.count_x-1 and 0 < j and j < norminput.count_y-1:
+            tmp = []
+            tmp_index = []
+            for ii in range(-1, 2):
+                for jj in range(-1, 2):
+                   tmp.append(norminput.data[i + ii][j + jj][0])
+                   tmp_index.append([i + ii, j + jj])
+            max_index = tmp.index(max(tmp))
+            if math.ceil(norminput.data[tmp_index[max_index][0]][tmp_index[max_index][1]][0]) != 0:
+                obst.append([i, j, math.ceil(norminput.data[tmp_index[max_index][0]][tmp_index[max_index][1]][0])])
+        elif norminput.data[i][j][0] != 0:
             obst.append([i, j, math.ceil(norminput.data[i][j][0])])
 
 s = Sci.SciSearch([50, 50, 50], [start_x, start_y, start_z], [dist_x, dist_y, dist_z], obst, norminput.filename_i) #(サイズ,始点,終点,障害物,モード)
